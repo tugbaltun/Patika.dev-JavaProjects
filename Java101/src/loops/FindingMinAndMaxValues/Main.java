76931964fdf0f501dcb850fd35b7707b31ba2766
Tugba Altun <36510900+tugbaltun@users.noreply.github.com>
2022-11-15 02:43:51 +0300

    FindingMinAndMaxValues command lines added

--- Java101/src/loops/FindingMinAndMaxValues/Main.java
+++ Java101/src/loops/FindingMinAndMaxValues/Main.java
@@ -3,7 +3,11 @@ package loops.FindingMinAndMaxValues;
 import java.util.Scanner;
 
 public class Main {
+
+    //Defining Scanner as global variable
     public static Scanner input = new Scanner(System.in);
+
+    //Printing console to if numbers are min or max
     public static void findMaxAndMin(int numberSize){
         int min = 0, max = 0, number = 0;
         for(int i = 0; i < numberSize; i++){
@@ -17,10 +21,12 @@ public class Main {
         System.out.println("En buyuk sayi: "+ max);
         System.out.println("En kucuk sayi: "+ min);
     }
+
     public static void main(String[] args) {
 
         System.out.print("Kac tane sayi gireceksiniz?");
         int numberSize = input.nextInt();
+        //Calling the method which will find max and min values
         findMaxAndMin(numberSize);
 
     }
