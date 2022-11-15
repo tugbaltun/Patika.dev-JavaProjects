0b14409304a9b76a6c2570d93050a21c4be95c96
Tugba Altun <36510900+tugbaltun@users.noreply.github.com>
2022-11-16 02:31:56 +0300

    ProgramToFindTheSumOfOddNumbers added

---
+++ Java101/src/loops/Practices/ProgramToFindTheSumOfOddNumbers.java
@@ -0,0 +1,19 @@
+package loops.Practices;
+
+import java.util.Scanner;
+
+public class ProgramToFindTheSumOfOddNumbers {
+    public static void main(String[] args) {
+        int num, sum = 0;
+        System.out.println("Lütfen sayilari girin (Tek sayi girilene kadar deva eder):");
+        Scanner sc = new Scanner(System.in);
+        do {
+            num = sc.nextInt();
+            if (num % 4 == 0) {
+                sum = sum + num;
+            }
+        } while (num % 2 == 0);
+
+        System.out.println("Dort sayisinin katlarinin toplami: " + sum);
+    }
+}
